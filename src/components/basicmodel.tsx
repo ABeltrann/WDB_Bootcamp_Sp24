4b3b39154ff1e9713382be60e383f0ba2b3b3423
Alejandro Beltran <alejandrobeltran@berkeley.edu>
2024-03-12 15:11:49 -0700

    give input a type for setInput

--- src/components/basicmodel.tsx
+++ src/components/basicmodel.tsx
@@ -2,11 +2,8 @@ import { AddIcon } from "@chakra-ui/icons";
 import { IconButton, Button, Modal, ModalContent, ModalOverlay, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, FormControl, FormLabel, Input, ColorProps } from "@chakra-ui/react";
 import React, { Dispatch, PropsWithRef, SetStateAction, useId, useState } from "react";
 
-export default function BasicModel( { input }: { input: { value: string; }[]}, {setInput} : {setInput: React.SetStateAction<{
-  value: string;
-}[]>}) {
+export default function BasicModel( { input }: { input: { value: string; }[]}, {setInput} : {setInput: React.SetStateAction<{value: string;}[]>}) {
     const { isOpen, onOpen, onClose } = useDisclosure()
-  
     const initialRef = React.useRef(null)
     const finalRef = React.useRef(null)
     const [value, setValue] = React.useState('')
@@ -33,7 +30,7 @@ export default function BasicModel( { input }: { input: { value: string; }[]}, {
             </ModalBody>
   
             <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={()=> setInput((input) => [...input, { value: value }])}>
+              <Button colorScheme='blue' mr={3} onClick={()=> setInput((input: String[]) => [...input, { value: value }])}>
                 Save
               </Button>
               <Button onClick={onClose}>Cancel</Button>
